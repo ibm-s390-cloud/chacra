845ac1963576c52be4cb520883369c7d6e4a25bc
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2024-12-03 16:18:08 +0100

    ubuntu 24

--- config/dev.py
+++ config/dev.py
@@ -5,7 +5,7 @@ from chacra import models
 # Server Specific Configurations
 server = {
     'port': '8083',
-    'host': '172.23.230.157'
+    'host': '172.23.232.5'
 }
 
 # Pecan Application Configurations
