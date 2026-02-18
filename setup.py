bef1aad6cf1f2226761131a431601b66100f612a
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2026-02-18 11:35:48 +0100

    sqlalchemy==1.4.53

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license = "MIT",
     install_requires=[
         "pecan",
-        "sqlalchemy==1.3.0",
+        "sqlalchemy==1.4.53",
         "psycopg2-binary==2.9.9",
         "pecan-notario",
         "python-statsd",
